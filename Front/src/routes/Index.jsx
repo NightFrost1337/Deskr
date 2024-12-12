cb4d19fd217e8b29fd004a7d12185a63391657f4
NightFrost1337 <nightfrost2discord@outlook.fr>
2024-12-12 19:41:35 +0100

    added my react template

---
+++ Front/src/routes/Index.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex flex-col gap-12 items-center justify-center">
+            <h1 className="text-xl font-bold bg-black text-transparent bg-clip-text">
+                Deskr
+            </h1>
+            <Button onClick={() => navigate('/videoscreen')}><span className="mt-1">Start</span></Button>
+        </div>
+    )
+}
